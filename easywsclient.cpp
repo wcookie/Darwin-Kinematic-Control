28c55b28f01d927b0dcbddec884983f20b94d51c
Wyatt Cook <wcookeverton@gmail.com>
2017-05-01 16:08:33 -0500

    Changed stdint to cstdint

--- easywsclient.cpp
+++ easywsclient.cpp
@@ -53,13 +53,14 @@
     #include <sys/types.h>
     #include <unistd.h>
     //#include <stdint.h>
-        typedef __int8 int8_t;
+    #include <cstdint.h>
+/*        typedef __int8 int8_t;
         typedef unsigned __int8 uint8_t;
         typedef __int32 int32_t;
         typedef unsigned __int32 uint32_t;
         typedef __int64 int64_t;
         typedef unsigned __int64 uint64_t;
-    #ifndef _SOCKET_T_DEFINED
+ */   #ifndef _SOCKET_T_DEFINED
         typedef int socket_t;
         #define _SOCKET_T_DEFINED
     #endif
