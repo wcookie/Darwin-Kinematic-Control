311078a169d50121400ad6acfb068e96b1115430
Wyatt Cook <wcookeverton@gmail.com>
2017-05-02 15:58:10 -0500

    Moved stuff from .cpp to .hpp ; did some reformatting

--- easywsclient.cpp
+++ easywsclient.cpp
@@ -54,14 +54,14 @@
     #include <unistd.h>
     //#include <stdint.h>
     //#include <cstdint.h>
-#if defined __UINT32_MAX__ or UINT32_MAX
+/*#if defined __UINT32_MAX__ or UINT32_MAX
     #include <inttypes.h>
 #else
     typedef unsigned char uint8_t;
     typedef unsigned short uint16_t;
     //typedef unsigned long uint32_t;
     typedef unsigned long long uint64_t;
-#endif
+#endif */
     #ifndef _SOCKET_T_DEFINED
      typedef int socket_t;
      #define _SOCKET_T_DEFINED
