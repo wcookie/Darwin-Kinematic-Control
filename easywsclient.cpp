49ca9f9b8c95104665dd49d3f59a595fa3bda9d6
Wyatt Cook <wcookeverton@gmail.com>
2017-05-02 15:48:08 -0500

    Typo change

--- easywsclient.cpp
+++ easywsclient.cpp
@@ -60,7 +60,7 @@
     typedef unsigned char uint8_t;
     typedef unsigned short uint16_t;
     //typedef unsigned long uint32_t;
-    typedef unsigned long long uint64_t;A
+    typedef unsigned long long uint64_t;
 #endif
     #ifndef _SOCKET_T_DEFINED
      typedef int socket_t;
